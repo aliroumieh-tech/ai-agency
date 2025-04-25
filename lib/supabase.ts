2527f03576aa6f8de3f75940176a6bc906d5e94a
aliroumieh-tech <roumieh029@gmail.com>
2025-04-25 16:07:27 +0200

    add files

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
 
 // Initialize the Supabase client
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
+const supabaseUrl =
+	process.env.NEXT_PUBLIC_SUPABASE_URL ||
+	"https://arvnrbrrjjxtxvnmsoqh.supabase.co";
+const supabaseKey =
+	process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
+	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFydm5yYnJyamp4dHh2bm1zb3FoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU1ODY5OTEsImV4cCI6MjA2MTE2Mjk5MX0.f1z3OeoxIymNdBSx4R04zXB1Gsdq4wK9ALjs2qZNzTM";
 
 // Create a single Supabase client for the entire app
 export const supabase = createClient(supabaseUrl, supabaseKey);
