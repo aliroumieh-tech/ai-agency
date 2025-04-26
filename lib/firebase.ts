2ce8f044286530d0db43ffcf75e757348bf006a8
aliroumieh-tech <roumieh029@gmail.com>
2025-04-27 01:36:53 +0200

    add files

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,4 +1,4 @@
-import { initializeApp, getApps, getApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 
