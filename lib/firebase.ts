b29aa820ca1d4b6cbba672fcb4fe557a99b275fa
aliroumieh-tech <roumieh029@gmail.com>
2025-04-27 01:38:17 +0200

    add files

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -5,7 +5,7 @@ import { getAnalytics } from "firebase/analytics";
 const firebaseConfig = {
 	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
 	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
-	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+	projectId: "agency-7e955",
 	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
 	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
 	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
