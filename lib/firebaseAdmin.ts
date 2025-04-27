3900567852f6af84f927a3d2283ae88a998a29cb
aliroumieh-tech <roumieh029@gmail.com>
2025-04-27 19:10:20 +0200

    add files

--- lib/firebaseAdmin.ts
+++ lib/firebaseAdmin.ts
@@ -3,9 +3,17 @@ import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
 export interface ServiceAccount {
+	type: string;
 	project_id: string;
+	private_key_id: string;
 	private_key: string;
 	client_email: string;
+	client_id: string;
+	auth_uri: string;
+	token_uri: string;
+	auth_provider_x509_cert_url: string;
+	client_x509_cert_url: string;
+	universe_domain: string;
 }
 
 export const serviceAccount = {
