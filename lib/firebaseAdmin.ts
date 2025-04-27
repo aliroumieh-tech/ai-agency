35f32565b8df8852c9b8a85d82613384ba66920a
aliroumieh-tech <roumieh029@gmail.com>
2025-04-27 23:25:19 +0200

    add files

--- lib/firebaseAdmin.ts
+++ lib/firebaseAdmin.ts
@@ -1,7 +1,7 @@
 // utils/firebaseAdmin.ts
 import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
-import * as serviceAccount from "../service_acount.json";
+// import * as serviceAccount from "../service_acount.json";
 import * as admin from "firebase-admin";
 
 // interface Params {
@@ -30,20 +30,20 @@ import * as admin from "firebase-admin";
 // 	clientC509CertUrl: serviceAccount.client_x509_cert_url,
 // };
 
-// export const serviceAccount = {
-// 	type: process.env.NEXT_PUBLIC_FIREBASE_TYPE!,
-// 	project_id: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
-// 	private_key_id: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY_ID!,
-// 	private_key: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY!,
-// 	client_email: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL!,
-// 	client_id: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_ID!,
-// 	auth_uri: process.env.NEXT_PUBLIC_FIREBASE_AUTH_URI!,
-// 	token_uri: process.env.NEXT_PUBLIC_FIREBASE_TOKEN_URI!,
-// 	auth_provider_x509_cert_url:
-// 		process.env.NEXT_PUBLIC_FIREBASE_AUTH_PROVIDER_X509_CERT_URL!,
-// 	client_x509_cert_url: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_X509_CERT_URL!,
-// 	universe_domain: process.env.NEXT_PUBLIC_FIREBASE_UNIVERSE_DOMAIN!,
-// } ;
+export const serviceAccount = {
+	type: process.env.NEXT_PUBLIC_FIREBASE_TYPE!,
+	project_id: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+	private_key_id: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY_ID!,
+	private_key: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY!,
+	client_email: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL!,
+	client_id: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_ID!,
+	auth_uri: process.env.NEXT_PUBLIC_FIREBASE_AUTH_URI!,
+	token_uri: process.env.NEXT_PUBLIC_FIREBASE_TOKEN_URI!,
+	auth_provider_x509_cert_url:
+		process.env.NEXT_PUBLIC_FIREBASE_AUTH_PROVIDER_X509_CERT_URL!,
+	client_x509_cert_url: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_X509_CERT_URL!,
+	universe_domain: process.env.NEXT_PUBLIC_FIREBASE_UNIVERSE_DOMAIN!,
+};
 
 if (!getApps().length) {
 	initializeApp({
