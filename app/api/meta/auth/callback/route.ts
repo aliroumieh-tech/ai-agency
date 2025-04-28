7aeabc35b2031b8e59c91beb82e1bc41b2791db7
aliroumieh-tech <roumieh029@gmail.com>
2025-04-28 18:27:23 +0200

    add files

--- app/api/meta/auth/callback/route.ts
+++ app/api/meta/auth/callback/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { firebaseAdmin } from "../../../../../lib/firebaseAdmin";
 import { META } from "@/lib/config";
-
+//
 export async function GET(request: Request) {
 	const { searchParams } = new URL(request.url);
 	const code = searchParams.get("code");
